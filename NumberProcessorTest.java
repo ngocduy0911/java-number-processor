4d40e92e306cd83a95ff4a11f52bee03d62f6820
ngocduy0911 <ngongocduy09042005@gmail.com>
2026-04-17 07:17:36 +0700

    Add tests for path coverage (fix #2)
    Added additional tests to cover different paths in NumberProcessor.

--- NumberProcessorTest.java
+++ NumberProcessorTest.java
@@ -7,4 +7,18 @@ public class NumberProcessorTest {
     void testStatementCoverage() {
         assertEquals(3, NumberProcessor.processNumber(2));
     }
+    @Test
+void testPath1() {
+    assertEquals(0, NumberProcessor.processNumber(0));
+}
+
+@Test
+void testPath2() {
+    assertEquals(-1, NumberProcessor.processNumber(1));
+}
+
+@Test
+void testPath3() {
+    assertEquals(3, NumberProcessor.processNumber(5));
+}
 }
